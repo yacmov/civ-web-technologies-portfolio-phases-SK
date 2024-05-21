6b0c28f209e15d02d1698e5831255bca1c6ea4f5
Cedric <yacmov@gmail.com>
2024-05-21 10:16:41 +0800

    feat(phase-5): TailwindCSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+    './phase-*/*.html',
+    './pages/**/*s.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
